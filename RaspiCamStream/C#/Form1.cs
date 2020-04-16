615723dc0bd52a9ef257169de24541fb31432f00
DaniloRoss <62284476+DaniloRoss@users.noreply.github.com>
2020-04-16 16:32:50 +0200

    Insermineto using

--- RaspiCamStream/C#/Form1.cs
+++ RaspiCamStream/C#/Form1.cs
@@ -7,6 +7,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Net.Sockets;
+using System.Text.RegularExpressions;
 
 namespace RaspiCamStream
 {
