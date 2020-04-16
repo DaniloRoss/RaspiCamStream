c78535cb2c7bd953b5a74acbf1fa722ad84b0523
DaniloRoss <62284476+DaniloRoss@users.noreply.github.com>
2020-04-16 16:31:28 +0200

    Inserimento controlli servomotori

--- RaspiCamStream/C#/Form1.cs
+++ RaspiCamStream/C#/Form1.cs
@@ -32,5 +32,72 @@ namespace RaspiCamStream
             label1.Text= returndata;
             clientSocket.Close();
         }
+         //CONTROLLI CAM-----------------------------------------------
+        #region
+        private void Pb_up_MouseDown(object sender, MouseEventArgs e)
+        {
+            Timer_up.Start();
+        }
+
+        private void Pb_up_MouseUp(object sender, MouseEventArgs e)
+        {
+            Timer_up.Stop();
+        }
+
+        private void Timer_up_Tick(object sender, EventArgs e)
+        {
+            sendmessage("U");
+        }
+
+        private void Pb_down_MouseDown(object sender, MouseEventArgs e)
+        {
+            Timer_down.Start();
+        }
+
+        private void Pb_down_MouseUp(object sender, MouseEventArgs e)
+        {
+            Timer_down.Stop();
+        }
+
+        private void Timer_down_Tick(object sender, EventArgs e)
+        {
+            sendmessage("D");
+        }
+
+        private void Pb_right_MouseDown(object sender, MouseEventArgs e)
+        {
+            Timer_right.Start();
+        }
+
+        private void Pb_right_MouseUp(object sender, MouseEventArgs e)
+        {
+            Timer_right.Stop();
+        }
+
+        private void Timer_right_Tick(object sender, EventArgs e)
+        {
+            sendmessage("R");
+        }
+
+        private void Pb_left_MouseDown(object sender, MouseEventArgs e)
+        {
+            Timer_left.Start();
+        }
+
+        private void Pb_left_MouseUp(object sender, MouseEventArgs e)
+        {
+            Timer_left.Stop();
+        }
+
+        private void Timer_left_Tick(object sender, EventArgs e)
+        {
+            sendmessage("L");
+        }
+
+        private void Pb_center_Click(object sender, EventArgs e)
+        {
+            sendmessage("C");
+        }
     }
 }
+ #endregion
